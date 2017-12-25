ca0774b422287bd60880d3cdc217e25b6393ef82
Mel Rudick <melissarudick@MacBook-Air.domain>
2017-12-24 18:11:48 -0600

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,26 @@
 class FiguresController <ApplicationController
 
   get "/figures" do
-    binding.pry
     erb :'figures/index'
   end
+
+  get "/figures/new" do
+
+    erb :'figures/new'
+  end
+
+  post "/figures" do
+    @figure = Figure.create(params[:figure])
+    if !params[:title].empty?
+      title = Title.create(params[:title])
+      @figure.title << title
+    end
+    if !params[:landmark].empty?
+      landmark = Landmark.create(params[:landmark])
+      @figure.landmark << landmark
+    end
+
+    redirect "figures/#{@figure.id}"
+  end
+
 end
